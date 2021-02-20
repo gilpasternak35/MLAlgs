340dcf15a782a63ee39a4979c44563cbd55a89ef
gilpasternak35 <gilpasternak35@gmail.com>
2021-02-20 10:07:33 -0800

    Functional Decision Tree!!! That can't predict yet

--- python/src/models/decision_tree/node.py
+++ python/src/models/decision_tree/node.py
@@ -18,32 +18,22 @@ class Node(object):
     LEFT_CLASS = 0
     _COST_MIN = 0.05
     _FEATURE_RESAMPLES = 10
+    _ROOT_CLASS_STATE = 2
 
-    def __init__(self, data: np.ndarray,
-                 labels: np.ndarray,
-                 class_state: int = 2):   # Note: class state initialized to 2 for root node as it does not have its own
+    def __init__(self, class_state: int = _ROOT_CLASS_STATE):
         """Constructs a childless node given data and threshold"""
+        # Note: class state initialized to 2 for root as it does not have actual state
         self._left: Node = None
         self._right: Node = None
         self._threshold = None
-        self._data = data.T
         self._class_state = class_state
-        self._labels = labels
-
-    @property
-    def data(self):
-        return None if self._data is None else deepcopy(self._data)
-
-    @property
-    def labels(self):
-        return None if self._labels is None else deepcopy(self._labels)
 
     @property
     def left(self) -> Node:
         return copy(self._left)
 
     # TODO :: abstract set_left and set_right w/ setattr()
-    def set_left(self, child: Union[Node, List[Any]]) -> None:
+    def set_left(self, child: Union[Node, Any]) -> None:
         """Sets the left child of this node
 
             :param child: either a constructed node or data to construct a
@@ -56,13 +46,13 @@ class Node(object):
             # TODO :: replace this w/ better method once figured out
             setattr(self._left, '_class_state', self.LEFT_CLASS)
         else:
-            self._left = Node(data=child, class_state=self.LEFT_CLASS)
+            self._left = Node(class_state=self.LEFT_CLASS)
 
     @property
     def right(self) -> Node:
         return copy(self._right)
 
-    def set_right(self, child: Union[Node, List[Any]]) -> None:
+    def set_right(self, child: Union[Node, Any]) -> None:
         """Sets the right child of this node
 
             :param child: either a constructed node or data to construct a
@@ -75,12 +65,12 @@ class Node(object):
             # TODO :: replace this w/ better method once figured out
             setattr(self._right, '_class_state', self.RIGHT_CLASS)
         else:
-            self._right = Node(data=child, class_state=self.RIGHT_CLASS)
+            self._right = Node(class_state=self.RIGHT_CLASS)
 
     def __str__(self) -> str:
-        return f'Node with data: \n{str(self._data)}\n and labels \n{str(self._labels)}\n'
+        return f'Node with state: {self._class_state} and threshold {str(self._threshold)}\n'
 
-    def _compute_threshold(self) \
+    def _compute_threshold(self, data: np.ndarray, labels: np.ndarray) \
             -> Dict[str, Union[Union[float, int, np.ndarray], Any]]:
         """Updates the threshold for this node"""
         optimal_cost = np.inf
@@ -88,14 +78,14 @@ class Node(object):
         optimal_indices_smaller = np.ndarray([])
         optimal_indices_larger = np.ndarray([])
 
-        for feature in self._data:
+        for feature in data:
             for i in range(self._FEATURE_RESAMPLES):
                 print(feature)
                 threshold = np.random.choice(feature, size=1)
-                indices_larger = np.where(feature >= threshold)
-                indices_smaller = np.where(feature < threshold)
-                impurity = GiniImpurity.compute(self._labels[indices_larger],
-                                                self._labels[indices_smaller])
+                indices_larger = np.where(feature >= threshold)[0]  # Todo: find a more elegant way to do this
+                indices_smaller = np.where(feature < threshold)[0]
+                impurity = GiniImpurity.compute(labels[indices_larger],
+                                                labels[indices_smaller])
                 if impurity < optimal_cost:
                     optimal_cost = impurity
                     optimal_threshold = threshold
@@ -104,7 +94,7 @@ class Node(object):
 
         return {
             'cost': optimal_cost,
-            'threshold': optimal_threshold,
+            'threshold': optimal_threshold[0],
             'smaller': optimal_indices_smaller,
             'larger': optimal_indices_larger
         }
@@ -112,39 +102,54 @@ class Node(object):
     def split(self, remaining_depth: int, data: np.ndarray,
               labels: np.ndarray) -> Node:
         """Split nodes"""
-        cost, self._threshold, smaller_indices, larger_indices = \
-            self._compute_threshold(self._data)
 
-        # for feature in self._data:
-        #     feature_under_threshold = []
-        #     feature_over_threshold = []
-        #     for i in range(len(feature)):
-        #         if i
+        # While we haven't reached optimal, maximum depth, or empty data
+        if remaining_depth > 0 and len(labels) > 1:
 
+            # If shape of data does not match expected label length, transposing
+            if len(data[0]) != len(labels):
+                data = data.T
 
-        over_threshold = np.ndarray([])
-        under_threshold = np.ndarray([])
+            # If nonempty, compute thresholds for split
+            cost, self._threshold, smaller_indices, larger_indices = \
+                self._compute_threshold(data, labels).values()
 
-        if remaining_depth > 0 and cost > self._COST_MIN:
+            # If we have reached optimal value, this will be our last split
+            if cost > self._COST_MIN:
+                remaining_depth = 1
 
-            # left: TODO ::  make sure this part is actually gonna work oops
-            self.set_left(self._left.split(remaining_depth - 1),
-                          data=under_threshold)
+            # Numpy splitting along column axis
+            data_over_threshold = data[:, larger_indices]
+            data_under_threshold = data[:, smaller_indices]
+            labels_under_threshold = labels[smaller_indices]
+            labels_over_threshold = labels[larger_indices]
+
+            # If no new_left and new_right have been initialized, building Nodes
+            if self._left is None:
+                self.set_left(self.LEFT_CLASS)
+
+            if self._right is None:
+                self.set_right(self.RIGHT_CLASS)
+
+            self._left.split(remaining_depth - 1,
+                             data=data_over_threshold,
+                             labels=labels_over_threshold)
             # right:
-            self.set_right(self._right.split(remaining_depth - 1),
-                           data=over_threshold)
-        else:
-            # this is a leaf representing a class. ...return?
-            return self
+            self._right.split(remaining_depth - 1,
+                              data=data_under_threshold,
+                              labels=labels_under_threshold)
+
+        # Propagating back root node
+        return self
 
 
 if __name__ == "__main__":
-    data = pd.DataFrame({"feature1": [1,3,3,4,5,6,7],
-                         "feature2": [0,0,0,0,0,0,0],
-                         "feature3": [1,1,1,1,2,2,2],
-                         "feature4": [1,2,2,7,7,7,7]})
-    labels = pd.Series([0,0,1,1,1,1,1])
-    nd = Node(data=data.to_numpy(), labels=labels.to_numpy())
+    data = pd.DataFrame({"feature1": [1, 3, 3, 4, 5, 6, 7],
+                         "feature2": [0, 0, 0, 0, 0, 0, 0],
+                         "feature3": [1, 1, 1, 1, 2, 2, 2],
+                         "feature4": [1, 2, 2, 7, 7, 7, 7]})
+    labels = pd.Series([0, 0, 1, 1, 1, 1, 1])
+    nd = Node(class_state=2)
     print(nd)
-    print(nd._compute_threshold(_data=data.to_numpy().T, _labels=labels.to_numpy()))
-
+    tree = nd.split(data=data.to_numpy(), labels=labels.to_numpy(), remaining_depth=5)
+    print(tree)
